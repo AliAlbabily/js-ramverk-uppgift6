14b17f702d806ed958d3bba52f11f6e50d67eb97
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-05-12 20:57:36 +0200

    Handle the form and reset the values
    Co-Authored-By: DeGGiii <81147016+DeGGiii@users.noreply.github.com>

--- src/AddMovieForm.js
+++ src/AddMovieForm.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function AddMovieForm() {
+    const titleRef = useRef(null);
+    const gradeRef = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Prevents the default form submission behavior
+
+        // Access the input values using the refs
+        const title = titleRef.current.value;
+        const grade = gradeRef.current.value;
+
+        // Log the form values
+        console.log('Submitted Title:', title);
+        console.log('Submitted Grade:', grade);
+
+        // Clear the form after submission (optional)
+        event.target.reset();
+    };
+
     return (
-        <form id="new-movie-form">
+        <form id="new-movie-form" onSubmit={handleSubmit}>
             <fieldset>
                 <h1>Lägg till en film</h1>
                 <div className="form-group">
                     <label htmlFor="title">Titel:</label>
-                    <input type="text" className="form-control" id="title" placeholder="Titel här..." />
+                    <input type="text" className="form-control" id="title" placeholder="Titel här..." ref={titleRef} />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="grade">Betyg:</label>
-                    <select id="grade" className="form-control" defaultValue="0">
+                    <select id="grade" className="form-control" defaultValue="0" ref={gradeRef}>
                         <option value="0">Välj betyg här...</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -24,7 +42,7 @@ function AddMovieForm() {
                 </div>
 
                 <div className="form-group mt-2">
-                    <button id="save-movie" className="btn btn-success">
+                    <button id="save-movie" className="btn btn-success" type="submit">
                         Spara film
                     </button>
                 </div>
