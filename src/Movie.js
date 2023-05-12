499aa077f466bfc68ec3c6e4e99212bdaec8f7f6
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-05-12 20:14:21 +0200

    Added new images
    Co-Authored-By: DeGGiii <81147016+DeGGiii@users.noreply.github.com>

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import imageDelete from './images/delete.png';
+import imageStar from './images/star.png';
 
 function Movie({ movie }) {
     return (
         // <li data-grade="${movie.grade}" data-title="${movie.title}">
         //     {movie.title} - Grade: {movie.grade}
         // </li>
-        <li>test data</li>
+        <li>
+            test data
+            <img src={imageDelete} alt="Delete movie" className="delete-movie-icon" />
+            <img src={imageStar} alt="Delete movie" />
+        </li>
     );
 }
 
